dd1f28f7f26108f52cab72f5cde344a3cc69387a
Kiryl Tserpilouski <terpilovskikirill@gmail.com>
2024-07-08 11:50:41 +0200

    feat: add react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
+const ReactCompilerConfig = {
+  runtimeModule: '@/mycache',
+};
+
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
 });
