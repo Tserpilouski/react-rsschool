c148cf35437c45266fe65e30fe25d1fb39901962
Kiryl Tserpilouski <terpilovskikirill@gmail.com>
2024-08-01 15:11:41 +0200

    feat: add some new tests

--- src/views/ErrorPage.test.tsx
+++ src/views/ErrorPage.test.tsx
@@ -1,27 +1,66 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import ErrorPage from './ErrorPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import * as router from 'react-router-dom';
 
-// Mock useRouteError and isRouteErrorResponse
-vi.mock('react-router-dom', () => ({
-  useRouteError: vi.fn(),
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  isRouteErrorResponse: (error: any) => error && 'status' in error,
-}));
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useRouteError: vi.fn(),
+    isRouteErrorResponse: vi.fn(),
+  };
+});
 
 describe('ErrorPage', () => {
-  it('should render error details if it is a route error response', () => {
-    render(<ErrorPage />);
+  it('should display error details when there is a route error', async () => {
+    const mockError = {
+      status: 404,
+      statusText: 'Not Found',
+      data: {
+        message: 'Page not found',
+      },
+    };
 
-    // Check that the rendered output matches the mockError details
+    (
+      router.useRouteError as unknown as ReturnType<typeof vi.fn>
+    ).mockReturnValue(mockError);
+    (
+      router.isRouteErrorResponse as unknown as ReturnType<typeof vi.fn>
+    ).mockReturnValue(true);
+
+    render(
+      <Router>
+        <Routes>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    );
+
     expect(screen.getByText('Oops!')).toBeInTheDocument();
+    expect(screen.getByText('404')).toBeInTheDocument();
+    expect(screen.getByText('Not Found')).toBeInTheDocument();
+    expect(screen.getByText('Page not found')).toBeInTheDocument();
   });
 
-  it('should render a generic message if it is not a route error response', () => {
-    render(<ErrorPage />);
+  it('should display a generic error message when there is no route error', async () => {
+    (
+      router.useRouteError as unknown as ReturnType<typeof vi.fn>
+    ).mockReturnValue(null);
+    (
+      router.isRouteErrorResponse as unknown as ReturnType<typeof vi.fn>
+    ).mockReturnValue(false);
 
-    // Check that the rendered output matches the non-error case
+    render(
+      <Router>
+        <Routes>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    );
+
     expect(screen.getByText('Oops!')).toBeInTheDocument();
   });
 });
