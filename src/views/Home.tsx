c378b00666b5d7c6c35ece6f787a07695b123c30
Tserpilouski <terpilovskikirill@gmail.com>
2024-10-10 22:26:28 +0200

    chek config

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -63,7 +63,7 @@ class Home extends Component<object, HomeState> {
 
   render() {
     if (this.state.throwError) {
-      throw new Error('Test error thrown');
+      throw new Error('Test error');
     }
     return (
       <>
